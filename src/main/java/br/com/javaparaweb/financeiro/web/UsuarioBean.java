df89bbf035e4142484cfe10e1732c8484ea93657
evandrosuzart <evandrossuzart@outlook.com>
2018-02-19 10:51:00 -0300

    configurada a classe UsuarioBean, que é responsável pela instância da classe Usuario na camada de visã

---
+++ src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
@@ -0,0 +1,26 @@
+package br.com.javaparaweb.financeiro.web;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.javaparaweb.financeiro.usuario.Usuario;
+
+@ManagedBean(name="usuarioBean")
+@RequestScoped
+public class UsuarioBean {
+	private Usuario usuario = new Usuario();
+	private String confirmarSenha;
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public String getConfirmarSenha() {
+		return confirmarSenha;
+	}
+	public void setConfirmarSenha(String confirmarSenha) {
+		this.confirmarSenha = confirmarSenha;
+	}
+		
+}
