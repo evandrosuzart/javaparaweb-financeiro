8fccdba266546d4c5072564b3860ae85b41cc126
evandrosuzart <evandrossuzart@outlook.com>
2018-02-19 14:34:15 -0300

    criada a pagina usuario.xhtml, que contém o formulário responsável pelo cadastro de cadastro, e adicionado o método salvar da classse UsuarioBean

--- src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
+++ src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
@@ -1,9 +1,12 @@
 package br.com.javaparaweb.financeiro.web;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.javaparaweb.financeiro.usuario.Usuario;
+import br.com.javaparaweb.financeiro.usuario.UsuarioRN;
 
 @ManagedBean(name="usuarioBean")
 @RequestScoped
@@ -22,5 +25,25 @@ public class UsuarioBean {
 	public void setConfirmarSenha(String confirmarSenha) {
 		this.confirmarSenha = confirmarSenha;
 	}
+	
+	public String novo() {
+		this.usuario = new Usuario();
+		this.usuario.setAtivo(true);
+		return "/publico/usuario";
+	}
+	
+	public String salvar() {
+		FacesContext context = FacesContext.getCurrentInstance();
 		
+		String senha = this.usuario.getSenha();
+		
+		if(!senha.equalsIgnoreCase(this.getConfirmarSenha())) {
+			FacesMessage facesMessage = new FacesMessage("A senha não foi confirmada corretamente");
+			context.addMessage(null,facesMessage);
+			return null;
+		}
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.salvar(this.usuario);
+		return "usuariosucesso";
+	}
 }
