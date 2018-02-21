8ad48211fee212e01ed7b133d67186e49976839f
evandrosuzart <evandrossuzart@outlook.com>
2018-02-21 09:40:11 -0300

    criada a pagina que faz manutenção nos registros de usuários já esistentes na base de dados

--- src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
+++ src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
@@ -1,5 +1,7 @@
 package br.com.javaparaweb.financeiro.web;
 
+import java.util.List;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -13,6 +15,8 @@ import br.com.javaparaweb.financeiro.usuario.UsuarioRN;
 public class UsuarioBean {
 	private Usuario usuario = new Usuario();
 	private String confirmarSenha;
+	private List<Usuario> lista;
+	private String destinoSalvar;
 	public Usuario getUsuario() {
 		return usuario;
 	}
@@ -27,6 +31,8 @@ public class UsuarioBean {
 	}
 	
 	public String novo() {
+		
+		this.destinoSalvar = "usuariosucesso";
 		this.usuario = new Usuario();
 		this.usuario.setAtivo(true);
 		return "/publico/usuario";
@@ -45,5 +51,40 @@ public class UsuarioBean {
 		UsuarioRN usuarioRN = new UsuarioRN();
 		usuarioRN.salvar(this.usuario);
 		return "usuariosucesso";
+	}
+	
+	public String getDestinoSalvar() {
+		return destinoSalvar;
+	}
+	public void setDestinoSalvar(String destinoSalvar) {
+		this.destinoSalvar = destinoSalvar;
+	}
+	public String editar() {
+		this.confirmarSenha = this.usuario.getSenha();
+		return "/publico/usuario";
+	}
+	public String excluir() {
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.excluir(this.usuario);
+		return this.destinoSalvar;
+	}
+	
+	public String ativar() {
+		if(this.usuario.isAtivo()) {
+			this.usuario.setAtivo(false);
+		}else {
+			this.usuario.setAtivo(true);
+		}
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.salvar(this.usuario);
+		return null;
+	}
+	
+	public List<Usuario> getLista(){
+		if(this.lista==null) {
+			UsuarioRN usuarioRN = new UsuarioRN();
+			this.lista = usuarioRN.listar();
+		}
+		return this.lista;
 	}
 }
